257a4b58109910fd34d0529695166bcafb7201b5
Dillon <dillon.steyl.98@outlook.com>
2018-01-12 19:33:59 +1000

    Updated MyPyTutor Sample

--- MyPyTutor Sample/hello_world/test.py
+++ MyPyTutor Sample/hello_world/test.py
@@ -1,4 +1,4 @@
-from python_test_case import PythonTestCase
+from python_test_case import PythonTestCase, run_tests
 import unittest
 
 # Handle capturing what the students print
@@ -8,11 +8,16 @@ from unittest.mock import patch
 class HelloWorldTest(PythonTestCase):
 
     def test(self):
+        """ Should print 'Hello, World!' """
+        # The above docstring is used as the description
+        # of the test to the student
+
         with patch("sys.stdout", new=StringIO()) as output:
+
             # Import the module here, as the code will print as soon
             # as we import it
             import attempt
-            self.assertEqual(output.getvalue().strip(), "Python is cool!")
+            self.assertEqual(output.getvalue().strip(), "Hello, World!")
 
 if __name__ == "__main__":
-    unittest.main()
+    run_tests(HelloWorldTest)
