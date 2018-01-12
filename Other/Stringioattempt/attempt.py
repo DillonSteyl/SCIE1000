d9e162b6f37abe8ac8238a9cd830175aec8203d1
calculusfish <SaskiaVanR@hotmail.com>
2018-01-12 12:47:43 +1000

    Added StringIO attempt
    Attempt at catching output and checking other things at the same time

---
+++ Other/Stringioattempt/attempt.py
@@ -0,0 +1,51 @@
+class Employee(object):
+    """
+    A salaried employee.
+
+    """
+    def __init__(self, name, salary):
+        """
+        Initialise a new Employee instance.
+
+        Parameters:
+            name (str): The employee's name.
+            salary (float): The employee's annual salary.
+
+        """
+        self._name = name
+        self._salary = salary
+
+    def get_name(self):
+        """
+        (str) Return the name.
+
+        """
+        return self._name
+
+    def wage(self):
+        """
+        (float) Return the forgnightly wage.
+
+        """
+        return self._salary/26
+
+
+class Worker(Employee):
+
+    def __init__(self, name, salary, manager):
+        super().__init__(name, salary)
+        self._manager = manager
+
+    def get_manager(self):
+        return self._manager
+
+class Executive(Employee):
+    
+    # def __init__(self, name, salary, bonus):
+    #     super().__init__(name, salary)
+    #     self._bonus = bonus
+
+    def wage(self):
+        return super().wage() + self._bonus / 24
+
+print("I like python yaa!")
