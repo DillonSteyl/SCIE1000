15f92f8bbc7657e5af56a90480f010dffdb73c26
eshawsmith <35290664+eshawsmith@users.noreply.github.com>
2018-01-16 23:53:36 +1100

    Update content_exercise_1b_attempt.py

--- ContextBased/content_exercise_1b_attempt.py
+++ ContextBased/content_exercise_1b_attempt.py
@@ -10,8 +10,8 @@ def calc(x1, x2, y1, y2):
 t = array([0, 1, 2, 3, 4, 6, 10])
 c = array([0, 34, 48, 19, 8, 1.2, 0])
 
-# Define any variables here as necessary (Hint, you will need to do this!)
+# Define extra variables here as necessary: (You will need to do this!)
 
 # While loop to implement the trapezoidal rule for all the data
 while i <= 6:
-    # Your code here
+    # Your code here:
