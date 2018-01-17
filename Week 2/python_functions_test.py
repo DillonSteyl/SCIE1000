0c7c37a70282ea97ee2a3d06e8751bbfcee38695
Dillon <dillon.steyl.98@outlook.com>
2018-01-17 17:31:35 +1000

    Write Python Function Test for Week 2

---
+++ Week 2/python_functions_test.py
@@ -0,0 +1,22 @@
+from python_test_case import PythonTestCase, run_tests
+import unittest
+from unittest.mock import patch
+from contextlib import redirect_stdout
+from os import devnull
+from pylab import *
+import io
+
+f = io.StringIO()
+with redirect_stdout(f):
+    import attempt
+
+class Tests(PythonTestCase):
+
+	def test_output(self):
+		""" All proper values are printed """
+		out = f.getvalue()
+		expected = str(sqrt(6)) + "\n" + str(exp(3)) + "\n" + str(sin(2)) + "\n" + str(log(5)) + "\n"
+		self.assertEqual(out, expected)
+
+if __name__ == "__main__":
+    run_tests(Tests)
