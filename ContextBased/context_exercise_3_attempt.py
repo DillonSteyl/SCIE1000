808ec4ea297a19c31a5fb98da39c5de137b41262
eshawsmith <35290664+eshawsmith@users.noreply.github.com>
2018-01-25 14:38:47 +1100

    Update context_exercise_3_attempt.py

--- ContextBased/context_exercise_3_attempt.py
+++ ContextBased/context_exercise_3_attempt.py
@@ -3,7 +3,7 @@ from pylab import *
 Q = arange(0,10000,1)
 i = 0
 
-while i < 0 #Put appropriate bound here
+while i < 0 #Put appropriate loop bound here
     # Create your linear models here:
     S =
     D =
