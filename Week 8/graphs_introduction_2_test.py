4ee3c7ee69585d6f27d92f7b6e5977e9e51d5693
Dillon <dillon.steyl.98@outlook.com>
2018-02-06 00:12:46 +1000

    Updated Test

--- Week 8/graphs_introduction_2_test.py
+++ Week 8/graphs_introduction_2_test.py
@@ -3,10 +3,18 @@ matplotlib.use('Agg')
 
 from python_test_case import PythonTestCase, run_tests
 from pylab import *
+import sys
+from unittest.mock import patch, call
 
 import attempt
 
 class Tests(PythonTestCase):
+
+	def setUp(self):
+		try:
+			del sys.modules["attempt"]
+		except KeyError:
+			pass
 
 	def test_X_var(self):
 		""" X array has not been modified """
@@ -15,6 +23,9 @@ class Tests(PythonTestCase):
 	def test_Y_var(self):
 		""" Y array contains the correct values """
 		self.assertEqual(attempt.Y.tolist(), sin(attempt.X).tolist())
+		
+	def test_plot_called(self):
+		""" plot function called - only works if matplotlib.pyplot.plot is called, not just 'plot' """
 		
 # Run the unit tests
 if __name__ == "__main__":
