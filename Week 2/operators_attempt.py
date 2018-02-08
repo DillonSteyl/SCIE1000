a4d964ff3307b53e3d54b91e5206c4120d88dd1d
calculusfish <SaskiaVanR@hotmail.com>
2018-02-08 14:50:04 +1000

    Update operators_attempt.py

--- Week 2/operators_attempt.py
+++ Week 2/operators_attempt.py
@@ -8,3 +8,5 @@ c = eval(input("The value of c: "))
 
 x1 = #put your first expression for x here
 x2 = #put your second expression for x here, order not important
+
+print("The value of x1 is", x1, ", and the value of x2 is", x2)
