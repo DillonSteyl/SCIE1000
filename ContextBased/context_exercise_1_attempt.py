4f1645132df6b1f14f969246854b05a20a141abe
eshawsmith <35290664+eshawsmith@users.noreply.github.com>
2018-02-09 11:01:40 +1000

    Add files via upload

---
+++ ContextBased/context_exercise_1_attempt.py
@@ -0,0 +1,23 @@
+from pylab import *
+
+def calc(x1, x2, y1, y2):
+	# Your code here:
+
+
+# Arrays containing dye dilution method data
+t = array([0, 1, 2, 3, 4, 6, 10])
+c = array([0, 34, 48, 19, 8, 1.2, 0])
+
+
+# Define extra variables here as necessary: (You will need to do this!)
+
+
+# While loop to implement the trapezoidal rule for all the data
+while i <= 6:
+    # Your code here:
+
+    
+# Print the output here:
+
+
+# Write code to determine the total cardiac output below:
