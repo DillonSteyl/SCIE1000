3105cd94841416ef5fd9094eb2b73cc218324d7d
calculusfish <SaskiaVanR@hotmail.com>
2018-02-12 12:24:29 +1000

    Create context_exercise_8_attempt.py

---
+++ ContextBased/context_exercise_8_attempt.py
@@ -0,0 +1,18 @@
+
+
+from pylab import *
+
+P = 100 # initial population. Don't change this.
+k = # choose a value for the growth rate between 0.01 and 0.2
+n = # choose a value for the number of time steps between 5 and 20
+time = # make an array of values from 0 to n-1 (inclusive)
+
+exponential = # use exponential to store your population values for the exponential equation
+compounding = # use compounding to store your population values for the compounding equation
+
+# Use any method you like to fill the exponetial and compounding arrays. These arrays will be checked. 
+
+# put your print statements here
+
+# plot your graph here
+
