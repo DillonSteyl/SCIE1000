7d7cf3d2b207fc2fa8fb82fdbb30e9ec2195154a
calculusfish <SaskiaVanR@hotmail.com>
2018-02-14 08:59:11 +1000

    Create content_exercise_9c_attempt.py

---
+++ ContextBased/content_exercise_9c_attempt.py
@@ -0,0 +1,21 @@
+from pylab import *
+
+# This is the function you will be writing
+# Please do not change the name of the function or any of the input variables or order
+def runSimulation(weeks, N, a, b, d, v, stepSize, message, graph):
+    #initialise and fill SIRD arrays
+    
+    #if message is equal to 1, print the following messages:
+    print("The total number of deaths is", yourDeathValueHere)
+    print("The maximum number of people infected at once is", yourMaxValueHere)
+    
+    #if graph is equal to 1, plot the arrays
+  
+    return yourDeathValueHere
+
+
+
+
+
+# do not modify (this code is necessary for MyPyTutor to show output):
+savefig("output.png")
