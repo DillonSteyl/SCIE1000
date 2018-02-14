093832f367614b823e027518acf745a40f67afa0
calculusfish <SaskiaVanR@hotmail.com>
2018-02-15 07:56:18 +1000

    Update operators_attempt.py

--- Week 2/operators_attempt.py
+++ Week 2/operators_attempt.py
@@ -1,12 +1,7 @@
 from pylab import *
 
-#you will learn how this bit of code works in another exercise
-a = eval(input("The value of a: "))
-b = eval(input("The value of b: "))
-c = eval(input("The value of c: "))
-#but for now, do not change it
+# ask the user for input here
 
-#you can use the variables a, b, and c in your code
 x1 = #put your first expression for x here
 x2 = #put your second expression for x here, order not important
 
