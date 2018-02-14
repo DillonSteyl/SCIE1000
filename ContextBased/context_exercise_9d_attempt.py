e33f0f96e7ae37088e84d49ae0622d692b274a4e
calculusfish <SaskiaVanR@hotmail.com>
2018-02-14 10:35:32 +1000

    Create context_exercise_9d_attempt.py

---
+++ ContextBased/context_exercise_9d_attempt.py
@@ -0,0 +1,22 @@
+from pylab import *
+
+# your runSimulation function here
+
+
+weeks = 20
+N = 25000000
+a = 2.5
+b = 0.5
+d = 0.0001
+stepSize = 0.1
+V = # The Vaccination Rate Array - from 1000000 to 2000000 (inclusive) with steps of 50000
+D = # The Death Array - initialise as an array with the same size as V
+
+# use a while loop to fill the D array
+
+# plot your graph here
+
+
+
+# do not modify (this code is necessary for MyPyTutor to show output):
+savefig("output.png")
