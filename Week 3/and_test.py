82607739b503dbbcb9e85d1abb33f0b49ef6e213
calculusfish <SaskiaVanR@hotmail.com>
2018-02-15 11:44:43 +1000

    Create and_test.py

---
+++ Week 3/and_test.py
@@ -0,0 +1,83 @@
+from python_test_case import PythonTestCase, run_tests
+from unittest.mock import patch, call
+import sys
+from io import StringIO
+from pylab import *
+
+class Tests(PythonTestCase):
+
+    def setUp(self):
+        try:
+            del sys.modules["attempt"]
+        except KeyError:
+            pass
+			
+    def test_allowed(self):
+        """Student can take the course."""
+        user_input = ["1","1","0"]
+        with patch("builtins.input", side_effect=user_input) as input_call:
+            with patch("sys.stdout", new=StringIO()) as output:
+                import attempt
+                self.assertEqual(output.getvalue().strip(),"You can take this course.")
+				
+    def test_000(self):
+        """ Student has taken no courses"""
+        user_input = ["0","0","0"]
+        with patch("builtins.input", side_effect=user_input) as input_call:
+            with patch("sys.stdout", new=StringIO()) as output:
+                import attempt
+                self.assertEqual(output.getvalue().strip(),"You cannot take this course, sorry!")
+				
+    def test_001(self):
+        """ Student has taken only HELP2001"""
+        user_input = ["0","0","1"]
+        with patch("builtins.input", side_effect=user_input) as input_call:
+            with patch("sys.stdout", new=StringIO()) as output:
+                import attempt
+                self.assertEqual(output.getvalue().strip(),"You cannot take this course, sorry!")
+
+    def test_010(self):
+        """ Student has taken only HELP1500"""
+        user_input = ["0","1","0"]
+        with patch("builtins.input", side_effect=user_input) as input_call:
+            with patch("sys.stdout", new=StringIO()) as output:
+                import attempt
+                self.assertEqual(output.getvalue().strip(),"You cannot take this course, sorry!")
+
+    def test_011(self):
+        """ Student has taken HELP1500 and HELP2000"""
+        user_input = ["0","1","1"]
+        with patch("builtins.input", side_effect=user_input) as input_call:
+            with patch("sys.stdout", new=StringIO()) as output:
+                import attempt
+                self.assertEqual(output.getvalue().strip(),"You cannot take this course, sorry!")
+
+    def test_100(self):
+        """ Student has taken only HELP1000"""
+        user_input = ["1","0","0"]
+        with patch("builtins.input", side_effect=user_input) as input_call:
+            with patch("sys.stdout", new=StringIO()) as output:
+                import attempt
+                self.assertEqual(output.getvalue().strip(),"You cannot take this course, sorry!")
+
+    def test_101(self):
+        """ Student has taken only HELP1000 and HELP2001"""
+        user_input = ["1","0","1"]
+        with patch("builtins.input", side_effect=user_input) as input_call:
+            with patch("sys.stdout", new=StringIO()) as output:
+                import attempt
+                self.assertEqual(output.getvalue().strip(),"You cannot take this course, sorry!")
+                    
+    def test_111(self):
+        """ Student has taken all courses"""
+        user_input = ["1","1","1"]
+        with patch("builtins.input", side_effect=user_input) as input_call:
+            with patch("sys.stdout", new=StringIO()) as output:
+                import attempt
+                self.assertEqual(output.getvalue().strip(),"You cannot take this course, sorry!")
+		
+		
+# Run the unit tests
+if __name__ == "__main__":
+        run_tests(Tests)
+
