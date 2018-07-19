32f3dd435a2f4a398b92b4b6c928ab4162a1b6af
calculusfish <SaskiaVanR@hotmail.com>
2018-07-19 16:18:40 +1000

    Update brackets_2_attempt.py

--- Spotting_Errors/brackets_2_attempt.py
+++ Spotting_Errors/brackets_2_attempt.py
@@ -2,8 +2,8 @@ from pylab import *
 
 #This is a program to calculate the hypotenuse of a triangle
 
-a = eval(input("Enter the first side length of the triange: ")
-b = eval(input("Enter the second side length of the triange: ")
+a = float(input("Enter the first side length of the triangle: ")
+b = float(input("Enter the second side length of the triangle: ")
 
 h_squared = a**2 + b**2
 h = h_squared**(1/2)
