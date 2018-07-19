a8d8918678a0180e8109f1b54c1b609986cb83f2
calculusfish <SaskiaVanR@hotmail.com>
2018-07-19 21:03:02 +1000

    Create array_introduction_5_attempt.py

---
+++ Week 9/array_introduction_5_attempt.py
@@ -0,0 +1,10 @@
+from pylab import *
+
+array1 = #define the first array here
+array2 = #define the second array here
+
+length = #ask for input here
+
+array3 = #define the third array here
+
+#print all three arrays here, in order
