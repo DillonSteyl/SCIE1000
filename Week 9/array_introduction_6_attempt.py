e4e5ae59a9c661d5b2206ff8c3b2d0c3bb7e9f78
calculusfish <SaskiaVanR@hotmail.com>
2018-07-19 21:41:46 +1000

    Update array_introduction_6_attempt.py

--- Week 9/array_introduction_6_attempt.py
+++ Week 9/array_introduction_6_attempt.py
@@ -1,5 +1,10 @@
 from pylab import *
 
-squares = zeros(10)
-i = 0
-# while loop here:
+array1 = array([1,43,7,12,3,9,2,23,4,5,6,2,3,65,7,43,2,3,4,5,67,8,3,2,345,456,8,2,21,34])
+array2 = array([123,43,6467,12,234,68,2,4,5,5,5,23,543,4576,8678,4,2,3,4,34,53,4,4,4,68,8,8,21,5,67])
+
+win_loss_draw = #define the array here
+       
+# while loop here: (don't forget i = i+1)
+
+#print win_loss_draw here
