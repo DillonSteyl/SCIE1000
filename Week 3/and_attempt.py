8b0f0b24ec4c921d8216034ed885d3cf1da0a996
calculusfish <SaskiaVanR@hotmail.com>
2018-07-29 09:40:31 +1000

    Update and_attempt.py

--- Week 3/and_attempt.py
+++ Week 3/and_attempt.py
@@ -1,1 +1,6 @@
 from pylab import *
+
+
+HELP1000 = float(input("Type 1 if you have taken the course HELP1000, 0 otherwise"))
+HELP1500 = float(input("Type 1 if you have taken the course HELP1500, 0 otherwise"))
+HELP2001 = float(input("Type 1 if you have taken the course HELP2001, 0 otherwise"))
