cf77f054ae37b0e453fb6766d7b80dcbaf38ed26
calculusfish <SaskiaVanR@hotmail.com>
2018-07-29 09:40:53 +1000

    Update or_attempt.py

--- Week 3/or_attempt.py
+++ Week 3/or_attempt.py
@@ -1,1 +1,6 @@
 from pylab import *
+
+HELP2000 = float(input("Type 1 if you have taken HELP2000, and 0 otherwise: "))
+HELP2001 = float(input("Type 1 if you have taken HELP2001, and 0 otherwise: "))
+HELP2500 = float(input("Type 1 if you have taken HELP2500, and 0 otherwise: "))
+HELP3001 = float(input("Type 1 if you have taken HELP3001, and 0 otherwise: "))
