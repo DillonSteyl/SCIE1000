ed4fdbfeeb2c86c1c89775e602dd44ed1147559c
calculusfish <SaskiaVanR@hotmail.com>
2018-08-05 19:43:14 +1000

    Update loop_challenge_test.py

--- Week 4/loop_challenge_test.py
+++ Week 4/loop_challenge_test.py
@@ -18,13 +18,7 @@ class Tests(PythonTestCase):
         with patch("builtins.input", side_effect=user_input) as input_call:
             with patch("sys.stdout", new=StringIO()) as output:
                 import attempt
-                self.assertEqual(output.getvalue().strip(),"Team B has the most fans for month 1\n\
-                Team A has the most fans for month 2\nTeam B has the most fans for month 3\n\
-                Team A has the most fans for month 4\nTeam A has the most fans for month 5\n\
-                Team B has the most fans for month 6\nTeam A has the most fans for month 7\n\
-                Team A has the most fans for month 8\nTeam B has the most fans for month 9\n\
-                Team B has the most fans for month 10\nTeam A has the most fans for month 11\n\
-                Team B has the most fans for month 12\nBoth teams are equally popular.")
+                self.assertEqual(output.getvalue().strip(),"Team B has the most fans for month 1\nTeam A has the most fans for month 2\nTeam B has the most fans for month 3\nTeam A has the most fans for month 4\nTeam A has the most fans for month 5\nTeam B has the most fans for month 6\nTeam A has the most fans for month 7\nTeam A has the most fans for month 8\nTeam B has the most fans for month 9\nTeam B has the most fans for month 10\nTeam A has the most fans for month 11\nTeam B has the most fans for month 12\nBoth teams are equally popular.")
 				
     def test2(self):
         """ Team A is most popular with input 1000, 500, 4500, 4000"""
@@ -32,13 +26,7 @@ class Tests(PythonTestCase):
         with patch("builtins.input", side_effect=user_input) as input_call:
             with patch("sys.stdout", new=StringIO()) as output:
                 import attempt
-                self.assertEqual(output.getvalue().strip(),"Team A has the most fans for month 1\n\
-                Team A has the most fans for month 2\nTeam B has the most fans for month 3\n\
-                Team A has the most fans for month 4\nTeam A has the most fans for month 5\n\
-                Team A has the most fans for month 6\nTeam A has the most fans for month 7\n\
-                Team A has the most fans for month 8\nTeam B has the most fans for month 9\n\
-                Team A has the most fans for month 10\nTeam A has the most fans for month 11\n\
-                Team B has the most fans for month 12\nTeam A has the most fans for 9 months.")
+                self.assertEqual(output.getvalue().strip(),"Team A has the most fans for month 1\nTeam A has the most fans for month 2\nTeam B has the most fans for month 3\nTeam A has the most fans for month 4\nTeam A has the most fans for month 5\nTeam A has the most fans for month 6\nTeam A has the most fans for month 7\nTeam A has the most fans for month 8\nTeam B has the most fans for month 9\nTeam A has the most fans for month 10\nTeam A has the most fans for month 11\nTeam B has the most fans for month 12\nTeam A has the most fans for 9 months.")
 				
     def test3(self):
         """ Both teams are equally popular with input"""
@@ -46,13 +34,7 @@ class Tests(PythonTestCase):
         with patch("builtins.input", side_effect=user_input) as input_call:
             with patch("sys.stdout", new=StringIO()) as output:
                 import attempt
-                self.assertEqual(output.getvalue().strip(),"Team B has the most fans for month 1\n\
-                Team A has the most fans for month 2\nTeam B has the most fans for month 3\n\
-                Team B has the most fans for month 4\nTeam A has the most fans for month 5\n\
-                Team B has the most fans for month 6\nTeam B has the most fans for month 7\n\
-                Team A has the most fans for month 8\nTeam B has the most fans for month 9\n\
-                Team B has the most fans for month 10\nTeam B has the most fans for month 11\n\
-                Team B has the most fans for month 12\nTeam B has the most fans for 9 months.")
+                self.assertEqual(output.getvalue().strip(),"Team B has the most fans for month 1\nTeam A has the most fans for month 2\nTeam B has the most fans for month 3\nTeam B has the most fans for month 4\nTeam A has the most fans for month 5\nTeam B has the most fans for month 6\nTeam B has the most fans for month 7\nTeam A has the most fans for month 8\nTeam B has the most fans for month 9\nTeam B has the most fans for month 10\nTeam B has the most fans for month 11\nTeam B has the most fans for month 12\nTeam B has the most fans for 9 months.")
                 
                
                
@@ -62,13 +44,7 @@ class Tests(PythonTestCase):
         with patch("builtins.input", side_effect=user_input) as input_call:
             with patch("sys.stdout", new=StringIO()) as output:
                 import attempt
-                self.assertEqual(output.getvalue().strip(),"Team B has the most fans for month 1\n\
-                Team B has the most fans for month 2\nTeam A has the most fans for month 3\n\
-                Team A has the most fans for month 4\nBoth teams are equally popular for month 5\n\
-                Team A has the most fans for month 6\nTeam A has the most fans for month 7\n\
-                Team B has the most fans for month 8\nTeam A has the most fans for month 9\n\
-                Team B has the most fans for month 10\nTeam B has the most fans for month 11\n\
-                Team B has the most fans for month 12\nTeam B has the most fans for 6 months.")
+                self.assertEqual(output.getvalue().strip(),"Team B has the most fans for month 1\nTeam B has the most fans for month 2\nTeam A has the most fans for month 3\nTeam A has the most fans for month 4\nBoth teams are equally popular for month 5\nTeam A has the most fans for month 6\nTeam A has the most fans for month 7\nTeam B has the most fans for month 8\nTeam A has the most fans for month 9\nTeam B has the most fans for month 10\nTeam B has the most fans for month 11\nTeam B has the most fans for month 12\nTeam B has the most fans for 6 months.")
     
     
     def test5(self):
@@ -77,13 +53,7 @@ class Tests(PythonTestCase):
         with patch("builtins.input", side_effect=user_input) as input_call:
             with patch("sys.stdout", new=StringIO()) as output:
                 import attempt
-                self.assertEqual(output.getvalue().strip(),"Team B has the most fans for month 1\n\
-                Team A has the most fans for month 2\nTeam B has the most fans for month 3\n\
-                Team A has the most fans for month 4\nTeam A has the most fans for month 5\n\
-                Team A has the most fans for month 6\nTeam A has the most fans for month 7\n\
-                Team A has the most fans for month 8\nTeam B has the most fans for month 9\n\
-                Team B has the most fans for month 10\nBoth teams are equally popular for month 11\n\
-                Team B has the most fans for month 12\nTeam A has the most fans for 6 months.")
+                self.assertEqual(output.getvalue().strip(),"Team B has the most fans for month 1\nTeam A has the most fans for month 2\nTeam B has the most fans for month 3\nTeam A has the most fans for month 4\nTeam A has the most fans for month 5\nTeam A has the most fans for month 6\nTeam A has the most fans for month 7\nTeam A has the most fans for month 8\nTeam B has the most fans for month 9\nTeam B has the most fans for month 10\nBoth teams are equally popular for month 11\nTeam B has the most fans for month 12\nTeam A has the most fans for 6 months.")
                 
     def test6(self):
         """ Both teams are equally popular with input 0, 0, 10, 10"""
@@ -91,13 +61,7 @@ class Tests(PythonTestCase):
         with patch("builtins.input", side_effect=user_input) as input_call:
             with patch("sys.stdout", new=StringIO()) as output:
                 import attempt
-                self.assertEqual(output.getvalue().strip(),"Both teams are equally popular for month 1\n\
-                Both teams are equally popular for month 2\nBoth teams are equally popular for month 3\n\
-                Both teams are equally popular for month 4\nBoth teams are equally popular for month 5\n\
-                Both teams are equally popular for month 6\nBoth teams are equally popular for month 7\n\
-                Both teams are equally popular for month 8\nBoth teams are equally popular for month 9\n\
-                Both teams are equally popular for month 10\nBoth teams are equally popular for month 11\n\
-                Both teams are equally popular for month 12\nBoth teams are equally popular.")
+                self.assertEqual(output.getvalue().strip(),"Both teams are equally popular for month 1\nBoth teams are equally popular for month 2\nBoth teams are equally popular for month 3\nBoth teams are equally popular for month 4\nBoth teams are equally popular for month 5\nBoth teams are equally popular for month 6\nBoth teams are equally popular for month 7\nBoth teams are equally popular for month 8\nBoth teams are equally popular for month 9\nBoth teams are equally popular for month 10\nBoth teams are equally popular for month 11\nBoth teams are equally popular for month 12\nBoth teams are equally popular.")
 
    
 		
