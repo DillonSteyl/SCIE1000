a39700c0869b244d2b7867f305ee1d8916bdbcb7
calculusfish <SaskiaVanR@hotmail.com>
2018-08-05 19:47:05 +1000

    Update loop_challenge_attempt.py

--- Week 4/loop_challenge_attempt.py
+++ Week 4/loop_challenge_attempt.py
@@ -1,4 +1,4 @@
-from pylab impor *
+from pylab import *
 
 #Ask the user to give the amplitudes and middles. Do not edit these lines!
 amplitudeA = float(input("Enter the amplitude for Team A: "))
@@ -13,9 +13,3 @@ middleB = float(input("Enter the middle for Team B: "))
 
 #Write some if statements here to print which team is most popular.
 
-if scoreA>scoreB:
-    print("Team A has the most fans for", scoreA, "months.")
-elif scoreB>scoreA:
-    print("Team B has the most fans for", scoreB, "months.")
-else:
-    print("Both teams are equally popular.")
