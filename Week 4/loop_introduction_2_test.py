1b25c77d110134aca8e3d09dc428f2acc39be66d
calculusfish <SaskiaVanR@hotmail.com>
2018-08-09 11:27:12 +1000

    Update loop_introduction_2_test.py

--- Week 4/loop_introduction_2_test.py
+++ Week 4/loop_introduction_2_test.py
@@ -22,7 +22,7 @@ class Tests(PythonTestCase):
 		""" program correctly prints all the average temperatures for Cairns """
 		with patch("sys.stdout", new=StringIO()) as output:
 			import attempt
-			self.assertEqual(output.getvalue().strip(), "Average temperature for month 1 is 28.0\nAverage temperature for month 2 is        27.598076211353316\nAverage temperature for month 3 is 26.5\nAverage temperature for month 4 is 25.0\nAverage temperature for month 5 is 23.5\nAverage temperature for month 6 is 22.401923788646684\nAverage temperature for month 7 is 22.0\nAverage temperature for month 8 is 22.401923788646684\nAverage temperature for month 9 is 23.5\nAverage temperature for month 10 is 25.0\nAverage temperature for month 11 is 26.5\nAverage temperature for month 12 is 27.598076211353316")
+			self.assertEqual(output.getvalue().strip(), "Average temperature for month 1 is 28.0\nAverage temperature for month 2 is\n27.598076211353316\nAverage temperature for month 3 is 26.5\nAverage temperature for month 4 is 25.0\nAverage temperature for month 5 is 23.5\nAverage temperature for month 6 is 22.401923788646684\nAverage temperature for month 7 is 22.0\nAverage temperature for month 8 is 22.401923788646684\nAverage temperature for month 9 is 23.5\nAverage temperature for month 10 is 25.0\nAverage temperature for month 11 is 26.5\nAverage temperature for month 12 is 27.598076211353316")
 			
 # Run the unit tests
 if __name__ == "__main__":
