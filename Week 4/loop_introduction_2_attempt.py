25f96ba75e106ca271ae96e16014d648617b8742
Dillon Steyl <34499900+dsteyl@users.noreply.github.com>
2018-08-09 14:20:39 +1000

    Update loop_introduction_2_attempt.py

--- Week 4/loop_introduction_2_attempt.py
+++ Week 4/loop_introduction_2_attempt.py
@@ -1,8 +1,4 @@
 from pylab import *
 
 #put this print statement inside your while loop
-<<<<<<< HEAD
 print("Average temperature for month", i, "is", 3*sin((i+2)*2*pi/12)+25)
-=======
-print("Average temperature for month", i, "is", 3*sin((i+2)*2*pi/12)+25)
->>>>>>> e8dac170d3a81f327068f991cb2ae578dec722cf
