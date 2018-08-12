6beffffb67f6720b4c43a4f45c882d141f93f6a4
calculusfish <SaskiaVanR@hotmail.com>
2018-08-12 19:32:29 +1000

    Create array_functions_attempt.py

---
+++ Week 5/array_functions_attempt.py
@@ -0,0 +1,11 @@
+from pylab import *
+
+# array creation code
+A = array([i*3+j**4+i*j-k*20+32 for i in range(10) for j in range(10) for k in range(10)])
+# do not edit the code above
+
+mean = #your code here
+maximum = # your code here
+minimum = # your code here
+
+print("Mean:",mean,", Max:",maximum,", Min:",minimum)
