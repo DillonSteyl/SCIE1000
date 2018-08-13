8ef9ad43e7b480be22d500077be08ff896371f1d
calculusfish <SaskiaVanR@hotmail.com>
2018-08-13 12:12:55 +1000

    Update index_context_test.py

--- Week 5/index_context_test.py
+++ Week 5/index_context_test.py
@@ -20,49 +20,49 @@ class Tests(PythonTestCase):
 			self.assertEqual(output.getvalue().strip(), expected)
   
   
-  def test_venus_out(self):
+  	def test_venus_out(self):
 		""" Input: Venus. Expected Output: 18 13 5, Excellent choice! """
 		with patch("sys.stdout", new=StringIO()) as output:
 			import attempt
 			expected = "18 13 5\nExcellent choice!"
 			self.assertEqual(output.getvalue().strip(), expected)
       
-  def test_earth_out(self):
+  	def test_earth_out(self):
 		""" Input: Earth. Expected Output: 13 5 25, A popular destination! """
 		with patch("sys.stdout", new=StringIO()) as output:
 			import attempt
 			expected = "13 5 25\nA popular destination!"
 			self.assertEqual(output.getvalue().strip(), expected)
       
-  def test_mars_out(self):
+  	def test_mars_out(self):
 		""" Input: Mercury. Expected Output: 5 25 20, Excellent choice! """
 		with patch("sys.stdout", new=StringIO()) as output:
 			import attempt
 			expected = "5 25 20\nExcellent choice!"
 			self.assertEqual(output.getvalue().strip(), expected)
       
-  def test_jupiter_out(self):
+  	def test_jupiter_out(self):
 		""" Input: Jupiter. Expected Output: 25 20 17, Excelent choice! """
 		with patch("sys.stdout", new=StringIO()) as output:
 			import attempt
 			expected = "25 20 17\nExcellent choice!"
 			self.assertEqual(output.getvalue().strip(), expected)
       
-  def test_saturn_out(self):
+  	def test_saturn_out(self):
 		""" Input: Saturn. Output: 20 17 8, Excellent choice! """
 		with patch("sys.stdout", new=StringIO()) as output:
 			import attempt
 			expected = "20 17 8\nExcellent choice!"
 			self.assertEqual(output.getvalue().strip(), expected)
       
-  def test_uranus_out(self):
+  	def test_uranus_out(self):
 		""" Input: Uranus. Output: 17 8 7, A popular destination! """
 		with patch("sys.stdout", new=StringIO()) as output:
 			import attempt
 			expected = "17 8 7\nA popular destination!"
 			self.assertEqual(output.getvalue().strip(), expected)
       
-  def test_neptune_out(self):
+  	def test_neptune_out(self):
 		""" Input: Neptune. Output: 8 7, A popular destination! """
 		with patch("sys.stdout", new=StringIO()) as output:
 			import attempt
