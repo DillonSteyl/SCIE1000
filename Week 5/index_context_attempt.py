951b8020aa64f9d16376411ce82bba34d94722d2
calculusfish <SaskiaVanR@hotmail.com>
2018-08-13 11:45:42 +1000

    Create index_context_attempt.py

---
+++ Week 5/index_context_attempt.py
@@ -0,0 +1,10 @@
+from pylab import *
+
+# costs to visit each planet (in billions of dollars)
+costs = array([18, 13, 5, 25, 20, 17, 8, 7])
+
+planet = float(input("Which planet would you like to visit (0 for Mercury, 1 for Venus, etc): "))
+
+#print prices
+
+#print message
