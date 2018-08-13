608dbf58df5d08b7e95f07be575554699df91137
calculusfish <SaskiaVanR@hotmail.com>
2018-08-13 11:50:24 +1000

    Update index_context_attempt.py

--- Week 5/index_context_attempt.py
+++ Week 5/index_context_attempt.py
@@ -3,7 +3,8 @@ from pylab import *
 # costs to visit each planet (in billions of dollars)
 costs = array([18, 13, 5, 25, 20, 17, 8, 7])
 
-planet = float(input("Which planet would you like to visit (0 for Mercury, 1 for Venus, etc): "))
+# this input statement uses int instead of float, it will work mostly the same way. Do not change it
+planet = int(input("Which planet would you like to visit (0 for Mercury, 1 for Venus, etc): "))
 
 #print prices
 
