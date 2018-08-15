fdbd7fd40b745b6a016f0fdf18beb58df7d78dcd
calculusfish <SaskiaVanR@hotmail.com>
2018-08-15 13:29:07 +1000

    Update array_functions_test.py

--- Week 5/array_functions_test.py
+++ Week 5/array_functions_test.py
@@ -6,7 +6,27 @@ class Tests(PythonTestCase):
 
 	def test_mean(self):
 		""" average has the correct value """
-		self.assertEqual(attempt.average, [1,4,9,16,25])
+		self.assertEqual(attempt.average, 1509.05)
+		
+	def test_maximum(self):
+		""" maximum has the correct value """
+		self.assertEqual(attempt.maximum, 6701)
+		
+	def test_minimum(self):
+		""" minimum has the correct value """
+		self.assertEqual(attempt.minimum, -148)
+		
+	def test_total(self):
+		""" total has the correct value """
+		self.assertEqual(attempt.total, 1509050)
+		
+	def test_length(self):
+		""" length has the correct value """
+		self.assertEqual(attempt.length, 1000)
+		
+	def test_difference(self):
+		""" difference has the correct value """
+		self.assertEqual(attempt.difference, 6849)
 	
 # Run the unit tests
 if __name__ == "__main__":
