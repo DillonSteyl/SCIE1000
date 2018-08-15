01bc39f99a4b7774aed900e79d3e0a561a30660a
calculusfish <SaskiaVanR@hotmail.com>
2018-08-15 15:25:16 +1000

    Update week5context_1_attempt.py

--- Week 5/week5context_1_attempt.py
+++ Week 5/week5context_1_attempt.py
@@ -4,6 +4,9 @@ trial_days = 30 # A variable you might wish to use in your code
 group1 = array([100.0,110.0,120.0,130.0,140.0,150.0])
 group2 = array([100.0,110.0,120.0,130.0,140.0,150.0])
 group3 = array([100.0,110.0,120.0,130.0,140.0,150.0])
-growth_diff = # Define Array Here
 
-#Write your code below
+# edit the group arrays here
+
+growth_diff = # define growth_diff array here
+
+
