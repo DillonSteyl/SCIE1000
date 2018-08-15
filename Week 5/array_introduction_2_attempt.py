7caf0fbc855cfeb83da0f36154b4d2cbde31014f
calculusfish <SaskiaVanR@hotmail.com>
2018-08-15 15:10:38 +1000

    Update array_introduction_2_attempt.py

--- Week 5/array_introduction_2_attempt.py
+++ Week 5/array_introduction_2_attempt.py
@@ -1,9 +1,2 @@
 from pylab import *
 
-exams = array([82, 48, 55, 67])
-assignments = array([90, 83, 64, 88])
-attendance = array([100, 80, 50, 70])
-
-final = #put your equation here
-
-#put your print message here
