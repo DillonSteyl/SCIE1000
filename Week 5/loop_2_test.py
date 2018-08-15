bd2a94a8abec14224f242667feadae7c270b5eeb
calculusfish <SaskiaVanR@hotmail.com>
2018-08-15 14:55:25 +1000

    Update loop_2_test.py

--- Week 5/loop_2_test.py
+++ Week 5/loop_2_test.py
@@ -8,7 +8,7 @@ class Tests(PythonTestCase):
 		""" 'height' array contains the correct heights for the plant """
 		self.assertEqual(attempt.height.tolist(), [10.0, 16.0, 19.0, 15.0, 18.0, 29.0, 35.0, 28.0, 34.0, 54.0, 65.0, 52.0])
     
-  def test_var(self):
+ 	def test_var(self):
 		""" 'height' array has the correct length (12)"""
 		self.assertEqual(12, len([10.0, 16.0, 19.0, 15.0, 18.0, 29.0, 35.0, 28.0, 34.0, 54.0, 65.0, 52.0]))
 	
