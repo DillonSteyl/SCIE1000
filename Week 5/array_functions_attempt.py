114fd415e888a0cae9a8f5e006cec37d7076332e
calculusfish <SaskiaVanR@hotmail.com>
2018-08-15 14:53:36 +1000

    Update array_functions_attempt.py

--- Week 5/array_functions_attempt.py
+++ Week 5/array_functions_attempt.py
@@ -1,7 +1,7 @@
 from pylab import *
 
 # array creation code
-A = array([i*3+j**4+i*j-k*20+32 for i in range(10) for j in range(10) for k in range(10)])
+bigArray = array([i*3+j**4+i*j-k*20+32 for i in range(10) for j in range(10) for k in range(10)])
 # do not edit the code above
 
 average = #your code here
