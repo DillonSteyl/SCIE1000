34ec9e54f9b34425451bad5825c62b2db889e99d
calculusfish <SaskiaVanR@hotmail.com>
2018-08-15 13:23:20 +1000

    Update array_functions_attempt.py

--- Week 5/array_functions_attempt.py
+++ Week 5/array_functions_attempt.py
@@ -4,8 +4,9 @@ from pylab import *
 A = array([i*3+j**4+i*j-k*20+32 for i in range(10) for j in range(10) for k in range(10)])
 # do not edit the code above
 
-mean = #your code here
-maximum = # your code here
-minimum = # your code here
-
-print("Mean:",mean,", Max:",maximum,", Min:",minimum)
+average = #your code here
+maximum = #your code here
+minimum = #your code here
+total = #your code here
+length = #your code here
+difference = #your code here
