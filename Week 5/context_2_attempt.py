60b7c57cfdf1396e2aa3b094c4056cd6cd66d9e7
calculusfish <SaskiaVanR@hotmail.com>
2018-08-15 12:38:29 +1000

    Create context_2_attempt.py

---
+++ Week 5/context_2_attempt.py
@@ -0,0 +1,18 @@
+from pylab import #
+
+# initialise friendA and friendB
+
+# fill in the first values for both arrays
+
+# fill in all the values
+
+# calculate the average, max, and sum
+print("Friend A -- Avg:", avgA, "Max:", maxA, "Sum:", sumA)
+print("Friend B -- Avg:", avgB, "Max:", maxB, "Sum:", sumB)
+
+# calculate the diff array
+print("The maximum difference is", maxDiff)
+
+#calculate the slopes
+
+#print which party is most popular
