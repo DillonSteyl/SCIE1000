ea7dad3b8542ac07ecac84898f5676113252270a
calculusfish <SaskiaVanR@hotmail.com>
2018-08-15 15:22:30 +1000

    Update week5context_1_test.py

--- Week 5/week5context_1_test.py
+++ Week 5/week5context_1_test.py
@@ -19,7 +19,7 @@ class Tests(PythonTestCase):
 		
     def test_group2(self):
         """ 'group2' array contains the correct values """
-        self.assertEqual(attempt.group2.tolist(), [134.78489153, 148.26338069, 161.74186984, 175.22035899, 188.69884815, 202.1773373 ])
+        self.assertEqual(attempt.group2.tolist(), [134.78489153329062, 148.26338068661966, 161.74186983994872, 175.22035899327778, 188.69884814660685, 202.1773372999359])
         
     def test_group3(self):
         """ 'group3' array contains the correct values """
@@ -27,8 +27,7 @@ class Tests(PythonTestCase):
 
     def test_growth_diff(self):
         """ 'growth_diff' array contains the correct values """
-        self.assertEqual(attempt.growth_diff.tolist(), [16.78489153, 20.26338069, 23.74186984, 27.22035899, 30.69884815,
-       34.1773373 ])
+        self.assertEqual(attempt.growth_diff.tolist(), [16.78489153329062, 20.263380686619655, 23.74186983994872, 27.220358993277785, 30.69884814660685, 34.177337299935914])
 
 # Run the unit tests
 if __name__ == "__main__":
