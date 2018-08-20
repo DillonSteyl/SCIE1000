d535149f49bd0d3447838b4d3d0ea90886d89f85
calculusfish <SaskiaVanR@hotmail.com>
2018-08-20 17:11:56 +1000

    Update brackets_1_attempt.py

--- Spotting_Errors/brackets_1_attempt.py
+++ Spotting_Errors/brackets_1_attempt.py
@@ -5,9 +5,9 @@ def quad_formula_pos(a, b, c):
     ans_pos = ((-b+(b**2-4*a*c)**(1/2)/2*a)
     return(ans_pos)
 
-a = eval(input("the value of a: "))
-b = eval(input("the value of b: "))
-c = eval(input("the value of c: "))
+a = float(input("the value of a: "))
+b = float(input("the value of b: "))
+c = float(input("the value of c: "))
 
 # quadratic formula
 quad_pos = quad_formula_pos(a, b, c)
