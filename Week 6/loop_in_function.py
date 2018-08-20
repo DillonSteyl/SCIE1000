5eca09a272058afaca915135d5411e70e5327f5f
calculusfish <SaskiaVanR@hotmail.com>
2018-08-20 11:09:02 +1000

    Create loop_in_function.py

---
+++ Week 6/loop_in_function.py
@@ -0,0 +1,21 @@
+# Loops in functions
+
+You can also include loops in functions. Here is an example:
+
+```
+def mySum(n):
+    # adds up all the numbers from 1 to n
+    i = 1
+    total = 0
+    while i<=n:
+        total+=i
+        i+=1
+    return(n)
+
+```
+
+
+
+**Task:** Write a function that take a number as input, and returns the factorial of that number.
+
+**Hint:** The factorial of n is all the integers from 1 to n multiplied together. So the factorial of 3 is 1*2*3 = 6.  
