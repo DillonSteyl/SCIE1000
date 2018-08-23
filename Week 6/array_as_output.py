717ffc2d12e58dd65ca5eac7437b4f3154a1a891
calculusfish <SaskiaVanR@hotmail.com>
2018-08-24 09:40:57 +1000

    Create array_as_output.py

---
+++ Week 6/array_as_output.py
@@ -0,0 +1,23 @@
+# Arrays as output
+
+Just like how you can use arrays as input, you can also have arrays as output!
+
+**Task:** Write a function called `fill_array` that takes two numbers as input and returns an array as output. The first number should be the length of the array, and the second number should be the number that the array is filled with.
+
+Example: calling `fill_array(4, 5)` should return [5, 5, 5, 5].
+
+**Hint:** Remember that you can use the zeros function to create an array of any length, and you can use operations on arrays to change all the numbers in the array at once. 
+
+# Solution
+
+```
+from pylab import *
+
+def fill_array(length, num):
+    A = zeros(int(length))
+    A = A + num
+    return(A)
+
+
+```
+
