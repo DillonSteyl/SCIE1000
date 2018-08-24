9603c81edf2bfa6f11b2569121ae17c8f3b0a323
Shelley <s.barfoot@yahoo.com.au>
2018-08-24 15:33:51 +1000

    float

--- Week 6/devine_attempt.py
+++ Week 6/devine_attempt.py
@@ -3,6 +3,6 @@
 
 
 # Do not modify:
-sex = eval(input("Enter 1 for female, 0 for male: "))
-height = eval(input("Enter height in cm: "))
+sex = float(input("Enter 1 for female, 0 for male: "))
+height = float(input("Enter height in cm: "))
 print("Ideal weight:", IBW(sex,height), "kg.")
