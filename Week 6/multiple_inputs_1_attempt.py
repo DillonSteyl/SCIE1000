dcc3396a56bdd6c6f4e17bf756d935dc1fc4badf
calculusfish <SaskiaVanR@hotmail.com>
2018-08-24 12:42:13 +1000

    Update multiple_inputs_1_attempt.py

--- Week 6/multiple_inputs_1_attempt.py
+++ Week 6/multiple_inputs_1_attempt.py
@@ -3,6 +3,6 @@ from pylab import *
 def prod(x,y):
     return(x*y)
     
-a = eval(input("Give me the first number: "))
-b = eval(input("Give me the second number: "))
+a = float(input("Give me the first number: "))
+b = float(input("Give me the second number: "))
 # print product:
