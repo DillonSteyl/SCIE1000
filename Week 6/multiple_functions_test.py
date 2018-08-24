b34db0927e352bf0c58b6c0871ea0ca315de3d2f
calculusfish <SaskiaVanR@hotmail.com>
2018-08-24 10:21:28 +1000

    Update multiple_functions_test.py

--- Week 6/multiple_functions_test.py
+++ Week 6/multiple_functions_test.py
@@ -13,46 +13,69 @@ class Tests(PythonTestCase):
 		except KeyError:
 			pass
 
-	def test_function_defined(self):
-		""" Function 'powers' is defined """
+	def test_tokilos_defined(self):
+		""" Function 'toKilograms' is defined """
 		with patch("builtins.input", return_value="1") as input_call:
 			import attempt
-			self.assertMethodDefined(attempt, "powers", 1)
+			self.assertMethodDefined(attempt, "toKilograms", 1)
+			
+	def test_mars_defined(self):
+		""" Function 'weightOnMars' is defined """
+		with patch("builtins.input", return_value="1") as input_call:
+			import attempt
+			self.assertMethodDefined(attempt, "weightOnMars", 1)
+			
+	def test_moon_defined(self):
+		""" Function 'weightOnMoon' is defined """
+		with patch("builtins.input", return_value="1") as input_call:
+			import attempt
+			self.assertMethodDefined(attempt, "weightOnMoon", 1)
 			
 	def test_function_behaviour1(self):
-		""" Answer 0.1, 100, 1000 with input 10 """
+		""" Answer 1 with call toKilograms(2.2) """
 		with patch("builtins.input", return_value="1") as input_call:
 			import attempt
-			a, b, c = attempt.powers(10)
-			self.assertEqual([a,b,c], [0.1, 100, 1000])
-
+			a = attempt.toKilograms(2.2)
+			self.assertEqual(a, 1.0)
+			
 	def test_function_behaviour2(self):
-		""" Answer 1, 1, 1 with input 1 """
+		""" Answer 70.0 with call toKilograms(154) """
 		with patch("builtins.input", return_value="1") as input_call:
 			import attempt
-			a, b, c = attempt.powers(1)
-			self.assertEqual([a,b,c], [1, 1, 1])
-      
-	def test_function_behaviour3(self):
-		""" Answer 0.5, 4, 8 with input 2 """
+			a = attempt.toKilograms(154)
+			self.assertEqual(a, 70.0)
+			
+	def test_function_behaviour1(self):
+		""" Answer 0.16 with call weightOnMoon(2.2) """
 		with patch("builtins.input", return_value="1") as input_call:
 			import attempt
-			a, b, c = attempt.powers(2)
-			self.assertEqual([a,b,c], [0.5, 4, 8])
-      
-	def test_function_behaviour4(self):
-		""" Answer 0.25, 16, 64 with input 4 """
+			a = attempt.weightOnMoon(2.2)
+			self.assertEqual(a, 0.16)
+			
+	def test_function_behaviour1(self):
+		""" Answer 0.38 with call weightOnMars(2.2) """
 		with patch("builtins.input", return_value="1") as input_call:
 			import attempt
-			a, b, c = attempt.powers(4)
-			self.assertEqual([a,b,c], [0.25, 16, 64])
-      
-	def test_function_behaviour5(self):
-		""" Answer 0.2, 25, 125 with input 5 """
+			a = attempt.weightOnMars(2.2)
+			self.assertEqual(a, 0.38)
+			
+	def test_function_behaviour1(self):
+		""" Answer 8.16 with call weightOnMoon(112.2) """
 		with patch("builtins.input", return_value="1") as input_call:
 			import attempt
-			a, b, c = attempt.powers(5)
-			self.assertEqual([a,b,c], [0.2, 25, 125])
+			a = attempt.weightOnMoon(112.2)
+			self.assertEqual(a, 8.16)
+			
+	def test_function_behaviour1(self):
+		""" Answer 19.38 with call weightOnMars(112.2) """
+		with patch("builtins.input", return_value="1") as input_call:
+			import attempt
+			a = attempt.weightOnMars(112.2)
+			self.assertEqual(a, 19.38)
+			
+	
+
+
 				
 # Run the unit tests
 if __name__ == "__main__":
