dbf8a699a1b56b5c9f8970866214aa01a7008fcd
eshawsmith <35290664+eshawsmith@users.noreply.github.com>
2018-09-02 21:59:47 +1000

    Add files via upload

---
+++ Week 8/graph_step_size_attempt.py
@@ -0,0 +1,15 @@
+from pylab import *
+
+# Create your time arrays here:
+t1 =
+t2 =
+t3 = 
+# Create your models for position here:
+x1 =
+x2 =
+x3 = 
+# Produce your plot here:
+
+
+# Do Not Modify Below This Line
+savefig("output.png")
