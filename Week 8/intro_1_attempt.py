912a7aca349ca04cef3cca1562cd93814979b2b4
calculusfish <SaskiaVanR@hotmail.com>
2018-09-02 14:25:26 +1000

    Create intro_1_attempt.py

---
+++ Week 8/intro_1_attempt.py
@@ -0,0 +1,14 @@
+from pylab import *
+
+
+
+# plot your graph here
+
+# don't forget the show function!
+
+
+
+
+
+# do not modify (this code is necessary for MyPyTutor to display the graph for you):
+savefig("output.png")
