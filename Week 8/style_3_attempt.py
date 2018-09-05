0a0c1d04409a1d7161d1ce5fff59f7d145f8f575
calculusfish <SaskiaVanR@hotmail.com>
2018-09-05 10:52:16 +1000

    Create style_3_attempt.py

---
+++ Week 8/style_3_attempt.py
@@ -0,0 +1,19 @@
+from pylab import *
+
+# x-coordinates
+X = arange(-2*pi, 2*pi, 0.05)
+# function Y1
+Y1 = X**2
+# function Y2
+
+# Plot the Y1 line first
+plot(X,Y1)
+# plot the Y2 line second
+
+
+# Add titles and axis labels
+
+
+#add a legend and grid lines
+
+show()
