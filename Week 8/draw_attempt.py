40d44f1baccfa5a50307785d3814c95c190e4471
calculusfish <SaskiaVanR@hotmail.com>
2018-09-05 11:48:15 +1000

    Create draw_attempt.py

---
+++ Week 8/draw_attempt.py
@@ -0,0 +1,10 @@
+from pylab import *
+
+# define your x and y coordinates
+
+# plot your graph
+
+# do not forget the show function
+
+# do not modify (this code is necessary for MyPyTutor to display the graph for you):
+savefig("output.png")
