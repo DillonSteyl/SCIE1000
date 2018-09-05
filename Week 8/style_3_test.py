bb4dafc37dfbfd2d6576932c96bd56a07dd0f319
calculusfish <SaskiaVanR@hotmail.com>
2018-09-05 11:20:07 +1000

    updated style_3_test

--- Week 8/style_3_test.py
+++ Week 8/style_3_test.py
@@ -32,32 +32,32 @@ class Tests(PythonTestCase):
 		
         g = gca()
         lines = g.get_lines()    
-		X = arange(-2*pi, 2*pi, 0.05)
+        X = arange(-2*pi, 2*pi, 0.05)
         self.assertEquals(lines[0].get_xdata(), X)
 		
-	def test_Y1(self):
+    def test_Y1(self):
         """The first line has the correct Y values"""
 		
         g = gca()
         lines = g.get_lines()    
-		X = arange(-2*pi, 2*pi, 0.05)
-		Y1 = X**2
+        X = arange(-2*pi, 2*pi, 0.05)
+        Y1 = X**2
         self.assertEquals(lines[0].get_ydata(), Y1)
 		
-	def test_Y2(self):
+    def test_Y2(self):
         """The second line has the correct Y values"""
 		
         g = gca()
         lines = g.get_lines()    
-		X = arange(-2*pi, 2*pi, 0.05)
-		Y2 = X**2 + 3*sin(12*X)
+        X = arange(-2*pi, 2*pi, 0.05)
+        Y2 = X**2 + 3*sin(12*X)
         self.assertEquals(lines[1].get_ydata(), Y2)
 		
-	def test_X2(self):
+    def test_X2(self):
         """The second line has the correct X values"""
         g = gca()
         lines = g.get_lines()    
-		X = arange(-2*pi, 2*pi, 0.05)
+        X = arange(-2*pi, 2*pi, 0.05)
         self.assertEquals(lines[1].get_xdata(), X)
 	
     def test_file_show(self):
@@ -67,14 +67,14 @@ class Tests(PythonTestCase):
             a = True
         self.assertEquals(a,True) 
 		
-	def test_file_grid(self):
+    def test_file_grid(self):
         """There are grid lines."""
         a = False
         if "grid(True)" in open('attempt.py').read():
             a = True
         self.assertEquals(a,True) 
 		
-	def test_file_legend(self):
+    def test_file_legend(self):
         """There is a legend"""
         a = False
         if "legend()" in open('attempt.py').read():
@@ -86,35 +86,35 @@ class Tests(PythonTestCase):
         g = gca() 
         self.assertEqual(g.get_xlabel, "x")
 		
-	def test_ylabel(self):
+    def test_ylabel(self):
         """The y axis is labelled as 'y'"""
         g = gca()
         self.assertEqual(g.get_ylabel, "y")
 		
-	def test_Y1_label(self):
+    def test_Y1_label(self):
         """The first line is labelled as 'y = x^2'"""
         g = gca()
         self.assertEqual(g.get_legend().get_texts()[0].gettext(), "y = x^2")
 		
-	def test_Y2_label(self):
+    def test_Y2_label(self):
         """The first line is labelled as 'y = x^2 + 3*sin(12x)'"""
         g = gca()
         self.assertEqual(g.get_legend().get_texts()[1].gettext(), "y = x^2 + 3*sin(12x)")
 		
-	def test_second_colour(self):
+    def test_second_colour(self):
         """The second line is red"""
         g = gca()
         lines = g.get_lines() 
         self.assertEqual(lines[1].get_color(), (1.0, 0.0, 0.0, 1))
 		
-	def test_first_colour(self):
+    def test_first_colour(self):
         """The first line is green"""
         g = gca()
         lines = g.get_lines() 
         self.assertEqual(lines[0].get_color(), (0.0, 0.5, 0.0, 1))
 		
-	def test_title(self):
-        """The title is "x^2 and x^2+3*sin(12x)""""
+    def test_title(self):
+        """The title is 'x^2 and x^2+3*sin(12x)'"""
         g = gca()
         self.assertEqual(g.get_title(), "x^2 and x^2+3*sin(12x)")
 		
