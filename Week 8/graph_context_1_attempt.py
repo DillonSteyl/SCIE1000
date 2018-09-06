c7ee695e48c5ad59514c8f5d70205abab976ee88
calculusfish <SaskiaVanR@hotmail.com>
2018-09-06 12:52:35 +1000

    Update graph_context_1_attempt.py

--- Week 8/graph_context_1_attempt.py
+++ Week 8/graph_context_1_attempt.py
@@ -1,9 +1,14 @@
 from pylab import *
 
+#these lines of code generate a random h value for you to use
+import random
+h = randint(0,114)
+
+
 # Some variables to use
 M = 3
 p = 0.5
-h = randint(0,140)
+
 
 # Write your program below:
 
