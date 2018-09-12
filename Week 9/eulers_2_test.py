b5a656c33eced24a463e8760d223567ffee28568
calculusfish <SaskiaVanR@hotmail.com>
2018-09-12 14:25:52 +1000

    Update eulers_2_test.py

--- Week 9/eulers_2_test.py
+++ Week 9/eulers_2_test.py
@@ -16,15 +16,31 @@ class Tests(PythonTestCase):
 			
 	def test_function_10(self):
 		""" Function 'fdash' returns "0.82436" for input "10" """
-		self.assertEqual(attempt.negate(10), 0.82436)
+		self.assertEqual(attempt.fdash(10), 0.82436)
 		
 	def test_function_zero(self):
 		""" Function 'fdash' returns "0" for input "0""""
-		self.assertEqual(attempt.negate(0), 0)
+		self.assertEqual(attempt.fdash(0), 0)
 		
 	def test_function_neg15(self):
 		""" Function 'fdash' returns "-0.35427" for input "-15" """
-		self.assertEqual(attempt.negate(-15), -0.35427)
+		self.assertEqual(attempt.fdash(-15), -0.35427)
+		
+	def test_function_defined2(self):
+		""" Function 'onestep' is defined """
+		self.assertMethodDefined(attempt, "onestep", 3)
+			
+	def test_function_111(self):
+		""" Function 'onestep' returns "1.05256" for input "1,1,1" """
+		self.assertEqual(attempt.pnestep(1,1,1), 1.05256)
+		
+	def test_function_101805(self):
+		""" Function 'onestep' returns "18.41218" for input "10,18,0.5" """
+		self.assertEqual(attempt.onestep(10,18,0.5), 18.41218)
+		
+	def test_function_1042036810(self):
+		""" Function 'onestep' returns "212.4265" for input "10.4,203.68,10" """
+		self.assertEqual(attempt.onestep(10.4,203.68,10), 212.4265)
 				
 # Run the unit tests
 if __name__ == "__main__":
