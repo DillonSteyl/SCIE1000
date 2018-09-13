619f373faa8970cdd57ac7cca18198767f6dcf04
calculusfish <SaskiaVanR@hotmail.com>
2018-09-13 10:07:20 +1000

    Update asteroid_test.py

--- Week 9/asteroid_test.py
+++ Week 9/asteroid_test.py
@@ -47,38 +47,38 @@ class Tests(PythonTestCase):
 		self.assertMethodDefined(attempt, "eulers", 4)
 			
 	def test_function_one(self):
-		""" Function 'eulers' returns "11.2223" for input "0,10,0.5,10" """
-		self.assertEqual(attempt.eulers(0,10,0.5,10), 11.2223)
+		""" Function 'eulers' returns "11.22736" for input "0,10,0.5,10" """
+		self.assertEqual(attempt.eulers(0,10,0.5,10), 11.22736)
 		
 	def test_function_two(self):
-		""" Function 'eulers' returns "12.26876" for input "12,12,0.1,12" """
-		self.assertEqual(attempt.eulers(12,12,0.1,12), 12.26876)
+		""" Function 'eulers' returns "12.269" for input "12,12,0.1,12" """
+		self.assertEqual(attempt.eulers(12,12,0.1,12), 12.269)
 		
 	def test_function_three(self):
-		""" Function 'eulers' returns "97.4465" for input "38.2,95.9,0.1,100" """
-		self.assertEqual(attempt.eulers(38.2,95.9,0.1,100), 97.4465)
+		""" Function 'eulers' returns "97.44892" for input "38.2,95.9,0.1,100" """
+		self.assertEqual(attempt.eulers(38.2,95.9,0.1,100), 97.44892)
 		
 	def test_function_defined4(self):
 		""" Function 'will_it_hit' is defined """
 		self.assertMethodDefined(attempt, "will_it_hit", 1)
 			
 	def test_function_big(self):
-		""" Function 'will_it_hit' returns "0, 149.65398" for input "1" """
+		""" Function 'will_it_hit' returns "0, 150.34594" for input "1" """
 		a, b = attempt.will_it_hit(1)
 		self.assertEqual(a, 0)
-		self.assertEqual(b, 149.65398)
+		self.assertEqual(b, 150.34594)
 		
 	def test_function_medium(self):
-		""" Function 'will_it_hit' returns "0, 149.96566" for input "0.1" """
+		""" Function 'will_it_hit' returns "0, 150.03482" for input "0.1" """
 		a, b = attempt.will_it_hit(0.1)
 		self.assertEqual(a, 0)
-		self.assertEqual(b, 149.96566)
+		self.assertEqual(b, 150.03482)
 		
 	def test_function_small(self):
-		""" Function 'will_it_hit' returns "1, 149.99671" for input "0.01" """
+		""" Function 'will_it_hit' returns "1, 150.00375" for input "0.01" """
 		a, b = attempt.will_it_hit(0.01)
 		self.assertEqual(a, 1)
-		self.assertEqual(b, 149.99671)
+		self.assertEqual(b, 150.00375)
 				
 # Run the unit tests
 if __name__ == "__main__":
