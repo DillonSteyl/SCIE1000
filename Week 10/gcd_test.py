29c06789185bd306197ce518a9d5e43f8495af32
calculusfish <SaskiaVanR@hotmail.com>
2018-09-28 10:43:34 +1000

    Update gcd_test.py

--- Week 10/gcd_test.py
+++ Week 10/gcd_test.py
@@ -14,54 +14,17 @@ class Tests(PythonTestCase):
 			pass
 
 	def test_function_defined(self):
-		""" Function 'travel' is defined """
+		""" Function 'gcd' is defined """
 		with patch("builtins.input", return_value="1") as input_call:
 			import attempt
-			self.assertMethodDefined(attempt, "travel", 2)
+			self.assertMethodDefined(attempt, "gcd", 2)
 		
-	def test0and50(self):
-		""" Function returns 1 with input (0, 50). Answer: month 2 -> season 1.  """
+	def test32and48(self):
+		""" Function returns 16 with input (32, 48). """
 		with patch("builtins.input", return_value="1") as input_call:
 			import attempt
-			a = attempt.travel(0, 50)
-			self.assertEqual(a, 1)
-			
-	def test5andneg6(self):
-		""" Function returns 0 with input (5, -6). Answer: month 11 -> season 0.  """
-		with patch("builtins.input", return_value="1") as input_call:
-			import attempt
-			a = attempt.travel(5, -6)
-			self.assertEqual(a, 0)
-			
-	def test8andneg136(self):
-		""" Function returns 1 with input (8, -136). Answer: month 4 -> season 1.  """
-		with patch("builtins.input", return_value="1") as input_call:
-			import attempt
-			a = attempt.travel(8, -136)
-			self.assertEqual(a, 10)
-			
-	def test9andneg1(self):
-		""" Function returns 0 with input (9, -1). Answer: month 8 -> season 3.  """
-		with patch("builtins.input", return_value="1") as input_call:
-			import attempt
-			a = attempt.travel(9, -1)
-			self.assertEqual(a, 3)
-			
-	def test6and0(self):
-		""" Function returns 1 with input (6, 0). Answer: month 6 -> season 2.  """
-		with patch("builtins.input", return_value="1") as input_call:
-			import attempt
-			a = attempt.travel(6, 0)
-			self.assertEqual(a, 2)
-			
-	def test7and1024(self):
-		""" Function returns 1 with input (7, 1024). Answer: month 11 -> season 0.  """
-		with patch("builtins.input", return_value="1") as input_call:
-			import attempt
-			a = attempt.travel(7, 1024)
-			self.assertEqual(a, 0)
-			
-			
+			a = attempt.gcd(32, 48)
+			self.assertEqual(a, 16)
 
 # Run the unit tests
 if __name__ == "__main__":
