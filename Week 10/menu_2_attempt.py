13fb7aa91ad33017c735e16f2292f556c88b775f
calculusfish <SaskiaVanR@hotmail.com>
2018-09-28 14:26:42 +1000

    Create menu_2_attempt.py

---
+++ Week 10/menu_2_attempt.py
@@ -0,0 +1,12 @@
+from pylab import *
+
+# ---Messages---
+
+# Print messages:
+# print("Your balance is", balance)
+# print("You have closed your bank account with a final balance of", balance)
+
+#Input messages:
+# float(input("Enter 1 for deposit, 2 for withdraw, and 3 to close the account"))
+# float(input("Enter the amount to deposit: "))
+# float(input("Enter the amount to withdraw: "))
