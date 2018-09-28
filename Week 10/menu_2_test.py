b41bb316f7d17381da027c222c3153470d5dd942
calculusfish <SaskiaVanR@hotmail.com>
2018-09-28 14:36:46 +1000

    Update menu_2_test.py

--- Week 10/menu_2_test.py
+++ Week 10/menu_2_test.py
@@ -11,60 +11,37 @@ class Tests(PythonTestCase):
 		except KeyError:
 			pass
 			
-	def testearlyexit(self):
-		""" User enters 0 and the program exits without printing anything."""
-		user_input = "0"
-		expected = ""
+				
+	def testscenario1(self):
+		""" User enters [100, 3] and the program exits with a final balance of 103"""
+		user_input = ["100","3"]
+		expected = "Your balance is 103.0\nYou have closed your bank account with a final balance of 103.0"
 		with patch("builtins.input", return_value=user_input) as input_call:
 			with patch("sys.stdout", new=StringIO()) as output:
 				import attempt
 				self.assertEqual(output.getvalue().strip(), expected)
 				
-	def testhiexit(self):
-		""" User enters [1, 0] and the program greets the user before exiting"""
-		user_input = ["1","0"]
-		expected = "Hey, how are you?"
+	def testscenario2(self):
+		""" User enters [100, 1, 50, 2, 20, 3] and the program exits with a final balance of 103"""
+		user_input = ["100","1", "50", "2", "20", "3"]
+		expected = "Your balance is 103.0\nYour balance is 157.59\nYour balance is 141.7177\n"
+		expected += "You have closed your bank account with a final balance of 141.7177"
 		with patch("builtins.input", return_value=user_input) as input_call:
 			with patch("sys.stdout", new=StringIO()) as output:
 				import attempt
 				self.assertEqual(output.getvalue().strip(), expected)
 				
-	def testnumexit(self):
-		""" User enters [2, 5, 0] and the program asks for a number, prints it, then exits """
-		user_input = ["2", "5", "0"]
-		expected = "Wow! 5.0 is a great number!"
-		with patch("builtins.input", return_value=user_input) as input_call:
-			with patch("sys.stdout", new=StringIO()) as output:
-				import attempt
-				self.assertEqual(output.getvalue().strip(), expected)
-	def testnumhiexit(self):
-		""" User enters [2, 5, 1, 0] and the program asks for a number, prints it, greets the user, then exits """
-		user_input = ["2", "5", "1", "0"]
-		expected = "Wow! 5.0 is a great number!\nHey, how are you?"
+	def testscenario3(self):
+		""" User enters [100, 2, 103, 2, 100, 2, 100, 3] and the program exits with a final balance of 103"""
+		user_input = ["100","2", "103", "2", "100", "2", "100", "3"]
+		expected = "Your balance is 103.0\nYour balance is 0.0\nYour balance is -103.0\nYour balance is -209.09\n"
+		expected += "You have closed your bank account with a final balance of -209.09"
 		with patch("builtins.input", return_value=user_input) as input_call:
 			with patch("sys.stdout", new=StringIO()) as output:
 				import attempt
 				self.assertEqual(output.getvalue().strip(), expected)
 				
-				
-	def testhi11exit(self):
-		""" User enters [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0] and the program greets the user 11 times, then exits """
-		user_input = ["1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "0"]
-		expected = "Hey, how are you?\n"*10 + "Hey, how are you?"
-		with patch("builtins.input", return_value=user_input) as input_call:
-			with patch("sys.stdout", new=StringIO()) as output:
-				import attempt
-				self.assertEqual(output.getvalue().strip(), expected)
-				
-	
-	def testnum3exit(self):
-		""" User enters [2, 5, 2, 8, 2, -132, 0] and the program asks and prints the users favourite number 3 times, then exits """
-		user_input = ["2", "5", "2", "8", "2", "-132", "0"]
-		expected = "Wow! 5.0 is a great number!\nWow! 8.0 is a great number!\nWow! -132.0 is a great number!"
-		with patch("builtins.input", return_value=user_input) as input_call:
-			with patch("sys.stdout", new=StringIO()) as output:
-				import attempt
-				self.assertEqual(output.getvalue().strip(), expected)
+
 				
 				
 				
