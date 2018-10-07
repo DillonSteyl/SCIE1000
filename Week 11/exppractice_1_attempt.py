4dd4dd7786171ec44f3a73ea98a9b362cbff8aa4
calculusfish <SaskiaVanR@hotmail.com>
2018-10-08 09:58:14 +1000

    Update exppractice_1_attempt.py

--- Week 11/exppractice_1_attempt.py
+++ Week 11/exppractice_1_attempt.py
@@ -3,10 +3,10 @@ from pylab import *
 # Since we are randomly generating the questions, we need to use the random library
 import random
 
-# this function will randomly generate an m value between -0.15 and 0.15 (but not too small)
+# this function will randomly generate an m value between -0.2 and 0.2 (but not too small)
 def generate_k():
-    k = round(random.random()*0.3-0.15, 3)
-    if abs(k)<0.05:
+    k = round(random.random()*0.4-0.2, 3)
+    if abs(k)<0.04:
         return generate_k()
     else:      
         return(k)
@@ -22,6 +22,7 @@ def generate_A0():
 def generate_data(k, A0):
     # the x values will be randomly chosen from 0 to 25
     x = array([random.randrange(25) for i in range(25)])
+    
     # the y values will be calculated using y = A0*exp(k*x) * a random number for scaling
     y = array([A0*exp(k*i)*(0.9+random.random()*0.2) for i in x])
     return x, y
