eceea6d53a84c70f19d27f6d0df3cf3a63eee2d5
calculusfish <SaskiaVanR@hotmail.com>
2018-10-08 11:06:03 +1000

    Update Exponential_or_Compounding_attempt.py

--- Week 11/Exponential_or_Compounding_attempt.py
+++ Week 11/Exponential_or_Compounding_attempt.py
@@ -1,5 +1,3 @@
-
-
 from pylab import *
 
 P = 100 # initial population. Don't change this.
@@ -7,15 +5,15 @@ k = # choose a value for the growth rate between 0.01 and 0.2
 n = # choose a value for the number of time steps between 30 and 50
 time = # make an array of values from 0 to n-1 (inclusive)
 
+# Use any method you like to fill in the exponetial and compounding arrays. These arrays will be checked. 
 exponential = # use exponential to store your population values for the exponential equation
 compounding = # use compounding to store your population values for the compounding equation
-
-# Use any method you like to fill the exponetial and compounding arrays. These arrays will be checked. 
 
 # put your print statements here
 
 # plot your graph here
 # remember to label your graph (x axis, y axis, and title)
+# include a legend!
 
 # do not modify (this code is necessary for MyPyTutor to show output):
 savefig("output.png")
