855bf4b9a7cbba2a77c0d922d231fb11c38c76bf
calculusfish <SaskiaVanR@hotmail.com>
2018-10-22 08:18:17 +1000

    Update Disease_X_Part_1_test.py

--- Week 13/Disease_X_Part_1_test.py
+++ Week 13/Disease_X_Part_1_test.py
@@ -24,39 +24,39 @@ class Tests(PythonTestCase):
         """Prints correct values."""
         expected = "The total number of deaths is 4943.620965569647\nThe maximum number of people infected at once is 12299582.792824812"
         out = f.getvalue()
-        self.assertEqual(out.strip(), expected)
+        self.assertAlmostEqual(out.strip(), expected)
 
     def test_susceptible_end(self):
         """Susceptible's last value is correct"""		
-        self.assertEqual(attempt.S[-1], 144987.81334934343)
+        self.assertAlmostEqual(attempt.S[-1], 144987.81334934343)
 
     def test_infected_end(self):
         """Infected's last value is correct """
-        self.assertEqual(attempt.I[-1], 131963.73783686993)
+        self.assertAlmostEqual(attempt.I[-1], 131963.73783686993)
 
     def test_recovered_end(self):
         """Recovered's last value is correct"""		
-        self.assertEqual(attempt.R[-1], 24718104.827848207)
+        self.assertAlmostEqual(attempt.R[-1], 24718104.827848207)
 
     def test_deceased_end(self):
         """Deceased's last value is correct"""
-        self.assertEqual(attempt.D[-1], 4943.620965569647)
+        self.assertAlmostEqual(attempt.D[-1], 4943.620965569647)
 
     def test_susceptible_start(self):
         """Susceptible's first value is correct"""		
-        self.assertEqual(attempt.S[0], 24999999)
+        self.assertAlmostEqual(attempt.S[0], 24999999)
 
     def test_infected_start(self):
         """Infected's first value is correct """
-        self.assertEqual(attempt.I[0], 1)
+        self.assertAlmostEqual(attempt.I[0], 1)
 
     def test_recovered_start(self):
         """Recovered's first value is correct"""		
-        self.assertEqual(attempt.R[0], 0)
+        self.assertAlmostEqual(attempt.R[0], 0)
 
     def test_deceased_start(self):
         """Deceased's first value is correct"""
-        self.assertEqual(attempt.D[0], 0)
+        self.assertAlmostEqual(attempt.D[0], 0)
 
     def test_lengths(self):
         """ time, S, I, R, and D all have the correct length """
