23af9c1d963924c643f7a11ab86a889920278fce
calculusfish <SaskiaVanR@hotmail.com>
2018-10-22 08:19:09 +1000

    Update Disease_X_Part_2_test.py

--- Week 13/Disease_X_Part_2_test.py
+++ Week 13/Disease_X_Part_2_test.py
@@ -61,35 +61,35 @@ class Tests(PythonTestCase):
 
     def test_susceptible_end(self):
         """Susceptible's last value is correct"""		
-        self.assertEqual(attempt.S[-1], 0)
+        self.assertAlmostEqual(attempt.S[-1], 0)
 
     def test_infected_end(self):
         """Infected's last value is correct """
-        self.assertEqual(attempt.I[-1], 163289.28738586543)
+        self.assertAlmostEqual(attempt.I[-1], 163289.28738586543)
 
     def test_recovered_end(self):
         """Recovered's last value is correct"""		
-        self.assertEqual(attempt.R[-1], 24834897.698995512)
+        self.assertAlmostEqual(attempt.R[-1], 24834897.698995512)
 
     def test_deceased_end(self):
         """Deceased's last value is correct"""
-        self.assertEqual(attempt.D[-1], 1813.0136186384832)
+        self.assertAlmostEqual(attempt.D[-1], 1813.0136186384832)
 
     def test_susceptible_start(self):
         """Susceptible's first value is correct"""		
-        self.assertEqual(attempt.S[0], 24999999)
+        self.assertAlmostEqual(attempt.S[0], 24999999)
 
     def test_infected_start(self):
         """Infected's first value is correct """
-        self.assertEqual(attempt.I[0], 1)
+        self.assertAlmostEqual(attempt.I[0], 1)
 
     def test_recovered_start(self):
         """Recovered's first value is correct"""		
-        self.assertEqual(attempt.R[0], 0)
+        self.assertAlmostEqual(attempt.R[0], 0)
 
     def test_deceased_start(self):
         """Deceased's first value is correct"""
-        self.assertEqual(attempt.D[0], 0)
+        self.assertAlmostEqual(attempt.D[0], 0)
 
     def test_lengths(self):
         """ time, S, I, R, and D all have the correct length """
