90a71c98d9078d5de9ef0a76300b70651f068655
calculusfish <SaskiaVanR@hotmail.com>
2019-02-15 15:35:23 +1000

    Create missing_brackets_3_attempt.py

---
+++ Week 12/missing_brackets_3_attempt.py
@@ -0,0 +1,24 @@
+from pylab import *
+
+print("This is a program to model the population of fish over time, with a growth rate of 0.05.")
+
+#growth value
+r = 0.05
+
+#initial number of fish
+init = float(input("Input the initial number of fish: "))
+
+#carrying capacity
+capacity = float(input("Input the carrying capacity of the fish population: "))
+
+#number of years to project for
+years = float(input("Input the number of years to be modelled: "))
+
+print("Year - Number of Fish")
+
+fish = init
+i=1
+while i <= years:
+    fish = init + r*init*(1-(init/capacity))
+    print(i, "  -  ", round(fish))
+    i = i + 1
