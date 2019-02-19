8f28c42a5b281aa1be7fb0a1288cb8a97dca4bc2
calculusfish <SaskiaVanR@hotmail.com>
2019-02-19 10:30:05 +1000

    Create conditionals_introduction_3_solution.py

---
+++ Week 3/conditionals_introduction_3_solution.py
@@ -0,0 +1,12 @@
+from pylab import *
+
+# Do not modify:
+input_radius = float(input("Enter radius of the circle: "))
+# Convert radius to a positive value, in case the input is negative:
+radius = abs(input_radius)
+area = pi*radius**2
+print("Area:", area)
+
+# Your code here:
+if radius == 1:
+    print("This is the unit circle!")
