bb5cd94599445730fc9cba7141940d1f08b4eac7
calculusfish <SaskiaVanR@hotmail.com>
2019-02-19 10:16:30 +1000

    Create variables_introduction_solution.py

---
+++ Week 2/variables_introduction_solution.py
@@ -0,0 +1,11 @@
+from pylab import *
+
+# create variables here:
+width = 13
+height = 22
+
+# output (no need to modify):
+area = width * height
+print("Width:", width)
+print("Height:", height)
+print("Area:", area)
