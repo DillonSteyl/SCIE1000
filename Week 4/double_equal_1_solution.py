844847ccc94f858c4b1be81ff961f98960a99b87
calculusfish <SaskiaVanR@hotmail.com>
2019-02-19 10:54:18 +1000

    Create double_equal_1_solution.py

---
+++ Week 4/double_equal_1_solution.py
@@ -0,0 +1,19 @@
+from pylab import *
+
+#A program to output whether a number is a perfect square number
+
+number = float(input("Enter a number: "))
+
+if number == 0:
+    print("Zero is a special case")
+
+elif number == 1:
+    print("One is a special case")
+    
+else:
+
+    i = 1.0
+    while i <= number/2:
+        if number / i == i: 
+            print("The number", number, "is a perfect square of", i)
+        i = i + 1
