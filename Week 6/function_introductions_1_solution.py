3fc2aa3002ed1d70f196bcc5fac777ceebe58926
calculusfish <SaskiaVanR@hotmail.com>
2019-02-19 11:13:43 +1000

    Create function_introductions_1_solution.py

---
+++ Week 6/function_introductions_1_solution.py
@@ -0,0 +1,11 @@
+from pylab import *
+
+def toCentimetres(inches):
+    # converts inches to centimetres
+    cm = inches * 2.54
+    return(cm)
+
+height_inches = float(input("Enter your height in inches: "))
+height_cm = toCentimetres(height_inches)
+
+print("You are", height_cm, "cm tall!")
