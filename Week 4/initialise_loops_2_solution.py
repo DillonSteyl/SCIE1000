79763f284e504de3e11058988004e171a76d113f
calculusfish <SaskiaVanR@hotmail.com>
2019-02-19 10:56:02 +1000

    Create initialise_loops_2_solution.py

---
+++ Week 4/initialise_loops_2_solution.py
@@ -0,0 +1,14 @@
+from pylab import *
+
+#calculating 2 to the power of x (input)
+x = float(input("Input the number to calculate 2 to the power of: "))
+
+power = 2
+
+if x > 0:
+    i = 1
+    while i < x:
+        power = power * 2.0
+        i = i + 1
+
+    print("2.0 to the power of", x, "is", power)
