01a66a7b037449115814220243d515d9fc7244f0
calculusfish <SaskiaVanR@hotmail.com>
2019-02-19 11:19:58 +1000

    Create variable_function_1_solution.py

---
+++ Week 6/variable_function_1_solution.py
@@ -0,0 +1,15 @@
+from pylab import *
+
+
+#functions for time dilation
+def time_dilation(t, V):
+    dilation = t * sqrt(1 - (V**2/299792458**2))
+    return(dilation)
+
+#input for time_dilation function
+time = float(input("Enter the time: "))
+velocity = float(input("Enter the velocity: "))
+
+dilation = time_dilation(time, velocity)
+
+print("The time dilation is", round(dilation))
