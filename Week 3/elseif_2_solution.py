661d321a4934ce5a980ce7c0de93ca0eb602a15d
calculusfish <SaskiaVanR@hotmail.com>
2019-02-19 10:34:26 +1000

    Create elseif_2_solution.py

---
+++ Week 3/elseif_2_solution.py
@@ -0,0 +1,9 @@
+from pylab import *
+
+number = float(input("Give me a number! "))
+if number < 0:
+    print("Don't be so negative!")
+elif number > 0:
+    print("Glad to see you're feeling positive!")
+else:
+    print("There's nothing here...")
