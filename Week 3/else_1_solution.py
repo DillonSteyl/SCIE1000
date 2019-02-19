51f353b07cc186cd1ac1241f1fab9ff4ee70162e
calculusfish <SaskiaVanR@hotmail.com>
2019-02-19 10:31:29 +1000

    Create else_1_solution.py

---
+++ Week 3/else_1_solution.py
@@ -0,0 +1,7 @@
+from pylab import *
+
+height = float(input("Enter your height. "))
+if height < 130:
+    print("You are not tall enough to ride!")
+else:
+    print("You are tall enough to ride.")
