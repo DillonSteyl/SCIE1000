1e0fe70c0ada769e12a57e9c38b7163153fc4cd2
calculusfish <SaskiaVanR@hotmail.com>
2019-02-19 10:13:04 +1000

    Create brackets_2_solution.py

---
+++ Week 2/brackets_2_solution.py
@@ -0,0 +1,11 @@
+from pylab import *
+
+#This is a program to calculate the hypotenuse of a triangle
+
+a = float(input("Enter the first side length of the triangle: "))
+b = float(input("Enter the second side length of the triangle: "))
+
+h_squared = a**2 + b**2
+h = h_squared**(1/2)
+
+print("The third side length of the triangle is", h)
