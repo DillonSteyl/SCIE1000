34cbfb1c4f58b3a9df5a5b524548f987529bad16
calculusfish <SaskiaVanR@hotmail.com>
2019-02-19 11:17:08 +1000

    Create multiple_inputs_1_solution.py

---
+++ Week 6/multiple_inputs_1_solution.py
@@ -0,0 +1,9 @@
+from pylab import *
+
+def prod(x,y):
+    return(x*y)
+    
+a = float(input("Give me the first number: "))
+b = float(input("Give me the second number: "))
+# print product:
+print( prod(a,b) )
