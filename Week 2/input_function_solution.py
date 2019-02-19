5ed78bcff0258a560bd480f67b46c388eb7b1fa6
calculusfish <SaskiaVanR@hotmail.com>
2019-02-19 10:14:07 +1000

    Create input_function_solution.py

---
+++ Week 2/input_function_solution.py
@@ -0,0 +1,9 @@
+from pylab import *
+
+# Take width as input:
+width = float(input("Enter width: "))
+# Take height as input:
+height = float(input("Enter height: "))
+
+area = width*height
+print("The area of the rectangle is:", area)
