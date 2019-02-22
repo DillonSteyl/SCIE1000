ff48c50218d107a296a09a25de2f500e5d0b15ad
calculusfish <SaskiaVanR@hotmail.com>
2019-02-22 15:51:41 +1000

    Create penguins_solution.py

---
+++ Week 13/penguins_solution.py
@@ -0,0 +1,31 @@
+from pylab import *
+
+def highest(A):
+    highestIndex = 0
+    i = 1
+    while i < len(A):
+        if A[i]> A[highestIndex]:
+            highestIndex = i
+        i = i + 1
+    return(highestIndex)
+
+# initialise the arrays
+F = zeros(16)
+P = zeros(16)
+
+# put in the initial values
+F[0] = 50
+P[0] = 600
+
+# start the while loop at 1, because the 0th element has already been filled in
+i = 1
+
+#fill in the arrays
+while i<16:
+    F[i] = max(-2*i*(i-8) + 50, 0)
+    P[i] = P[i-1]*0.90 + (P[i-1]/6) * (F[i-1]/100)
+    i = i + 1
+    
+maxIndex = highest(F)
+
+print("The quarter with the highest number of fish is", maxIndex, "with", P[maxIndex], "penguins on the island at the time.")
