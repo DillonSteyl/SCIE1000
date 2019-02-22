805cb087f9c073e85e8218c70ccf53a0a71b616b
calculusfish <SaskiaVanR@hotmail.com>
2019-02-22 20:01:47 +1000

    Create whale_attempt.py

---
+++ Week 13/whale_attempt.py
@@ -0,0 +1,21 @@
+from pylab import *
+
+steps = 201
+W = #your code here
+Z = #your code here
+
+# initialise the values for step 0
+W[0] = 12
+Z[0] = 40
+
+#your code here to fill in the W ans Z arrays
+
+    
+    
+maxIndexW = #your code here for the step number for the most whales
+maxIndexZ = #your code here for the step number for the most zooplankton
+
+print("The whale population reaches its peak of", round(yourcodehere),"whales during step", yourcodehere)
+print("The zooplankton population reaches its peak of", round(yourcodehere),"thousand tonnes during step", yourcodehere)
+
+print("The number of steps between these peaks is", yourcodehere)
