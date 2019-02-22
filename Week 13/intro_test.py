af316db934cc7e831b7c46bd82f6a5bba088987e
calculusfish <SaskiaVanR@hotmail.com>
2019-02-22 13:57:30 +1000

    Update intro_test.py

--- Week 13/intro_test.py
+++ Week 13/intro_test.py
@@ -14,26 +14,34 @@ class Tests(PythonTestCase):
 			pass
 
 	def test_function_defined(self):
-		""" Function 'toCentimetres' is defined """
+		""" Function 'highest' is defined """
 		with patch("builtins.input", return_value="1") as input_call:
 			import attempt
-			self.assertMethodDefined(attempt, "toCentimetres", 1)
+			self.assertMethodDefined(attempt, "highest", 1)
 		
-	def test_function_behaviour(self):
-		""" Function 'toCentimetres' behaves as expected """
+	def test_function_behaviour1(self):
+		""" Returns 0 with input [8] """
 		with patch("builtins.input", return_value="1") as input_call:
 			import attempt
-			test_val = randint(60,80)
-			self.assertEqual(attempt.toCentimetres(test_val), test_val*2.54)
-
-	def test_output(self):
-		""" Correctly uses the toCentimetres function to convert the inputted height """
-		user_input = randint(50,90)
-		with patch("builtins.input", return_value=str(user_input)) as input_call:
-			with patch("sys.stdout", new=StringIO()) as output:
-				import attempt
-				expected = "You are " + str(attempt.toCentimetres(user_input)) + " cm tall!"
-				self.assertEqual(output.getvalue().strip(), expected)
+			self.assertEqual(attempt.toCentimetres([8]), 0)
+			
+	def test_function_behaviour2(self):
+		""" Returns 2 with input [0, 0, 4, 1, 4, 4] """
+		with patch("builtins.input", return_value="1") as input_call:
+			import attempt
+			self.assertEqual(attempt.toCentimetres([0,0,4,1,4,4]), 2)
+			
+	def test_function_behaviour3(self):
+		""" Returns 4 with input [3.3, 82, 1.46, 1.66, 82.2, 4.354, -13] """
+		with patch("builtins.input", return_value="1") as input_call:
+			import attempt
+			self.assertEqual(attempt.toCentimetres([3.3, 82, 1.46, 1.66, 82.2, 4.354, -13]), 4)
+			
+	def test_function_behaviour4(self):
+		""" Returns 7 with input [2, 4, 6, 8, 10, 12, 14, 16] """
+		with patch("builtins.input", return_value="1") as input_call:
+			import attempt
+			self.assertEqual(attempt.toCentimetres([2,4,6,8,10,12,14,16]), 7)
 				
 # Run the unit tests
 if __name__ == "__main__":
