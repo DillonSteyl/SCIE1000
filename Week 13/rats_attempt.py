1c47884d931935a42085a8ada84f4e4743705dae
calculusfish <SaskiaVanR@hotmail.com>
2019-02-22 17:02:35 +1000

    Create rats_attempt.py

---
+++ Week 13/rats_attempt.py
@@ -0,0 +1,29 @@
+from pylab import *
+
+
+steps = 51 # a variable for you to use
+
+# initialise the arrays here
+
+# fill in the initial values
+
+# your while loop goes here
+
+   
+   
+
+print("The final number of rats in each cage are:")
+print("A -", round(yourcodehere))
+print("B -", round(yourcodehere))
+print("C -", round(yourcodehere))
+print("D -", round(yourcodehere))
+    
+maxIndexA = # your code here for the step that has the most rats in cage A
+maxIndexB = # your code here for the step that has the most rats in cage B
+maxIndexC = # your code here for the step that has the most rats in cage C
+maxIndexD = # your code here for the step that has the most rats in cage D
+
+print("The largest number of rats in a cage A is", round(yourcodehere),"at step", maxIndexA)
+print("The largest number of rats in a cage B is", round(yourcodehere),"at step", maxIndexB)
+print("The largest number of rats in a cage C is", round(yourcodehere),"at step", maxIndexC)
+print("The largest number of rats in a cage D is", round(yourcodehere),"at step", maxIndexD)
