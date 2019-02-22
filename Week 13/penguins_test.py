d274cd56d0fe28ee1b0f66db76d1c1c97f79e003
calculusfish <SaskiaVanR@hotmail.com>
2019-02-22 16:03:31 +1000

    Update penguins_test.py

--- Week 13/penguins_test.py
+++ Week 13/penguins_test.py
@@ -22,49 +22,31 @@ class Tests(PythonTestCase):
 
     def test_output(self):
         """Prints correct values."""
-        expected = "The total number of deaths is 4943.620965569647\nThe maximum number of people infected at once is 12299582.792824812 which occurred at step 100"
+        expected = "The quarter with the highest number of fish is 4 with 628.0 penguins on the island at the time."
         out = f.getvalue()
         self.assertAlmostEqual(out.strip(), expected)
 
-    def test_susceptible_end(self):
-        """Susceptible's last value is correct"""		
-        self.assertAlmostEqual(attempt.S[-1], 144987.81334934343)
+    def test_fish_end(self):
+        """The last value for the fish array is correct"""		
+        self.assertAlmostEqual(attempt.F[-1], 0)
 
-    def test_infected_end(self):
-        """Infected's last value is correct """
-        self.assertAlmostEqual(attempt.I[-1], 131963.73783686993)
+    def test_penguin_end(self):
+        """the last value for the penguin array is correct """
+        self.assertAlmostEqual(attempt.P[-1], 390.75510334276015)
 
-    def test_recovered_end(self):
-        """Recovered's last value is correct"""		
-        self.assertAlmostEqual(attempt.R[-1], 24718104.827848207)
+    def test_fish_start(self):
+        """The first value for the fish array is correct"""		
+        self.assertAlmostEqual(attempt.F[0], 50)
 
-    def test_deceased_end(self):
-        """Deceased's last value is correct"""
-        self.assertAlmostEqual(attempt.D[-1], 4943.620965569647)
-
-    def test_susceptible_start(self):
-        """Susceptible's first value is correct"""		
-        self.assertAlmostEqual(attempt.S[0], 24999999)
-
-    def test_infected_start(self):
-        """Infected's first value is correct """
-        self.assertAlmostEqual(attempt.I[0], 1)
-
-    def test_recovered_start(self):
-        """Recovered's first value is correct"""		
-        self.assertAlmostEqual(attempt.R[0], 0)
-
-    def test_deceased_start(self):
-        """Deceased's first value is correct"""
-        self.assertAlmostEqual(attempt.D[0], 0)
+    def test_penguin_end(self):
+        """The first value for the penguin array is correct"""
+        self.assertAlmostEqual(attempt.P[0], 600)
 
     def test_lengths(self):
-        """ time, S, I, R, and D all have the correct length """
-        self.assertTrue(size(attempt.time)==201
-                        and size(attempt.S)==201
-                        and size(attempt.I)==201
-                        and size(attempt.R)==201
-                        and size(attempt.D)==201)
+        """ The arrays all have the correct lengths """
+        self.assertEqual(size(attempt.P), 16)
+	self.assertEqual(size(attempt.F), 16)
+
 		
 # Run the unit tests
 if __name__ == "__main__":
