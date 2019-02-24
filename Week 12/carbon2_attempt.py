cacad69ebf6ac5416583af447275b0f099e2fbad
calculusfish <SaskiaVanR@hotmail.com>
2019-02-24 16:31:57 +1000

    Create carbon2_attempt.py

---
+++ Week 12/carbon2_attempt.py
@@ -0,0 +1,8 @@
+from pylab import *
+
+#your calculate_age function here
+
+# Messages for your use:
+# input message: "Please enter the percentage of isotopes from the sample found to be Carbon-14: "
+# wrong input message: "Please try again as this percentage is not valid."
+# answer message: "The number of years that have passed since the fossil was formed is approximately", round(years)
