4899df135c1172a9aa8ae4b6e342fa8136c40343
calculusfish <SaskiaVanR@hotmail.com>
2019-02-24 16:17:51 +1000

    Update carbon1_test.py

--- Week 12/carbon1_test.py
+++ Week 12/carbon1_test.py
@@ -14,26 +14,34 @@ class Tests(PythonTestCase):
 			pass
 
 	def test_function_defined(self):
-		""" Function 'toCentimetres' is defined """
+		""" Function 'calculate_age' is defined """
 		with patch("builtins.input", return_value="1") as input_call:
 			import attempt
-			self.assertMethodDefined(attempt, "toCentimetres", 1)
+			self.assertMethodDefined(attempt, "calculate_age", 1)
 		
-	def test_function_behaviour(self):
-		""" Function 'toCentimetres' behaves as expected """
+	def test_1(self):
+		""" The function returns the value 11460 with input 25 """
 		with patch("builtins.input", return_value="1") as input_call:
 			import attempt
-			test_val = randint(60,80)
-			self.assertEqual(attempt.toCentimetres(test_val), test_val*2.54)
-
-	def test_output(self):
-		""" Correctly uses the toCentimetres function to convert the inputted height """
-		user_input = randint(50,90)
-		with patch("builtins.input", return_value=str(user_input)) as input_call:
-			with patch("sys.stdout", new=StringIO()) as output:
-				import attempt
-				expected = "You are " + str(attempt.toCentimetres(user_input)) + " cm tall!"
-				self.assertEqual(output.getvalue().strip(), expected)
+			self.assertEqual(attempt.calculate_age(25), 11460)
+			
+	def test_2(self):
+		""" The function returns the value 5730 with input 50 """
+		with patch("builtins.input", return_value="1") as input_call:
+			import attempt
+			self.assertEqual(attempt.calculate_age(50), 5730)
+			
+	def test_3(self):
+		""" The function returns the value 17190 with input 12.5 """
+		with patch("builtins.input", return_value="1") as input_call:
+			import attempt
+			self.assertEqual(attempt.calculate_age(12.5), 17190)
+			
+	def test_4(self):
+		""" The function returns the value 13728.671315377427 with input 19 """
+		with patch("builtins.input", return_value="1") as input_call:
+			import attempt
+			self.assertAlmostEqual(attempt.calculate_age(19), 13728.671315377427)
 				
 # Run the unit tests
 if __name__ == "__main__":
