b68fb782b612a734256e870724e3786d8c3ca9d4
Shelley <s.barfoot@yahoo.com.au>
2019-02-28 15:04:46 +1000

    error in solution

--- Week 3/andorchallenge_solution.py
+++ Week 3/andorchallenge_solution.py
@@ -3,7 +3,7 @@ from pylab import *
 height = float(input("Enter your height: "))
 adult = float(input("Enter 1 if you are accompanied by an adult, and 0 otherwise"))
 
-if height<=200 and (height>=130 or (height>100 and adult==1)):
+if height<=200 and (height>=130 or (height>=100 and adult==1)):
     print("Enjoy your ride!")
 else:
     print("Sorry, you cannot ride this rollercoaster.")
